c2d94f09081f16f6083a14794a56c9246ef3367f
Tom Wardill <tom@howrandom.net>
2012-08-28 11:53:50 +0100

    Skeleton plugin

---
+++ hldsmonitor.py
@@ -0,0 +1,12 @@
+from SourceLib import SourceQuery
+
+class Hldsmonitor:
+	def __init__(self, agentConfig, mainLogger, rawConfig):
+		self.agentConfig = agentConfig
+		self.mainLogger = mainLogger
+		self.rawConfig = rawConfig
+
+	def run(self):
+
+		data['ping'] = 1
+		return data
