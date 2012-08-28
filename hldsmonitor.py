b7454dd3c73945732d8dd58f07f5dea41bfbc5a9
Tom Wardill <tom@howrandom.net>
2012-08-28 12:09:42 +0100

    Space tabs

--- hldsmonitor.py
+++ hldsmonitor.py
@@ -1,12 +1,12 @@
 from SourceLib import SourceQuery
 
 class Hldsmonitor:
-	def __init__(self, agentConfig, mainLogger, rawConfig):
-		self.agentConfig = agentConfig
-		self.mainLogger = mainLogger
-		self.rawConfig = rawConfig
+    def __init__(self, agentConfig, mainLogger, rawConfig):
+        self.agentConfig = agentConfig
+        self.mainLogger = mainLogger
+        self.rawConfig = rawConfig
 
-	def run(self):
-
-		data['ping'] = 1
-		return data
+    def run(self):
+        data = {}
+        data['ping'] = 1
+        return data
